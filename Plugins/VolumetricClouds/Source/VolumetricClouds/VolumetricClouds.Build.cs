d4bce5e19df2231539b80ae9bf02c22a3a6d4414
mikebru <michael.bruner3@gmail.com>
2019-09-18 22:59:18 -0500

    added in volumetric cloud system

---
+++ Plugins/VolumetricClouds/Source/VolumetricClouds/VolumetricClouds.Build.cs
@@ -0,0 +1,54 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class VolumetricClouds : ModuleRules
+{
+	public VolumetricClouds(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+                "VolumetricClouds/Public"
+            }  
+            );
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+                "VolumetricClouds/Private"
+            }  
+            );
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+                "UnrealEd",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
